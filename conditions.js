b93a3690bca72855028863065600599a64397541
Makhmud <yuldoshmakhmud1@gmail.com>
2026-09-12 23:05:08 +0900

    shart

---
+++ conditions.js
@@ -0,0 +1,33 @@
+// shart operatorlari
+if (5 > 3) {
+    console.log("5 3 dan katta");
+} else {
+    console.log("5 3 dan kichik");
+}
+
+// shart operatorlari 2 xil buladi if else va switch case
+let age = 18;
+if (age >= 18) {
+    console.log("siz voyaga yetgansiz");
+} else {
+    console.log("siz voyaga yetmagansiz");
+}
+
+// switch case
+let day = "Monday";
+switch (day) {
+    case "Monday":
+        console.log("Bugun dushanba");
+        break;
+    case "Tuesday":
+        console.log("Bugun seshanba");
+        break;
+    case "Wednesday":
+        console.log("Bugun chorshanba");
+        break;
+    default:
+        console.log("Bunday kun yoq");
+}   
+
+// if else va switch case farqi if else da shartlar bir hil buladi lekin switch case da shartlar bir hil bulmaydi   
+    
